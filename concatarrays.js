b73338ce839f3bcf626e209a9e1b5408b43974c2
rishabhtatia1 <rishabhtatia1@gmail.com>
2019-07-04 22:49:31 +0530

    adds assignment

---
+++ concatarrays.js
@@ -0,0 +1,11 @@
+/*Create a function to concatenate two integer arrays.
+
+Examples
+concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+
+concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+*/
+function concat(arr1, arr2) {
+	return arr1.concat(arr2);
+
+}
