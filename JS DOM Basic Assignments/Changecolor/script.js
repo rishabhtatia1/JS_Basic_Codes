097613444273022404ab8721c30be6dc83ce6e2c
rishabhtatia1 <rishabhtatia1@gmail.com>
2019-08-01 00:43:04 +0530

    DOM Assignments

---
+++ JS DOM Basic Assignments/Changecolor/script.js
@@ -0,0 +1,11 @@
+const colorBtn = document.querySelector('.colorBtn');
+const bodyBkg =  document.querySelector('body');
+
+const colors = ['blue', 'red', 'yellow', 'brown', '#3b5998', 'orange'];
+
+colorBtn.addEventListener('click',changeColor);
+
+function changeColor() {
+  let random = Math.floor(Math.random() * colors.length);
+  bodyBkg.style.backgroundColor = colors[random];
+}
---
+++ JS DOM Basic Assignments/Counter/script.js
@@ -0,0 +1,29 @@
+let counter = document.querySelector('.counter');
+const addCount = document.querySelector('#addCntBtn');
+const decCount = document.querySelector('#lowerCntBtn');
+let count = 0;
+addCount.addEventListener('click', incrementCount);
+decCount.addEventListener('click', decrementCount);
+
+function incrementCount() {
+  count++;
+  counter.innerHTML = count;
+  if (counter.innerHTML > '0') {
+    counter.style.color = 'green';
+  }
+  else if (counter.innerHTML === '0') {
+    counter.style.color = 'white';
+  }
+  counter.animate([{opacity:'0.2'}, {opacity:'1.0'}], {duration:500, fill:'forwards'});
+}
+function decrementCount() {
+  count--;
+  counter.innerHTML = count;
+  if (counter.innerHTML < '0') {
+    counter.style.color = 'red';
+  }
+  else if (counter.innerHTML === '0') {
+    counter.style.color = 'white';
+  }
+  counter.animate([{opacity:'0.2'}, {opacity:'1.0'}], {duration:500, fill:'forwards'});
+}
